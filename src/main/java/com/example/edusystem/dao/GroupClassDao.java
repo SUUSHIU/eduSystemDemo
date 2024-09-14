ca5fb0432560ff560ee70d1c3a6471c1111ad0e7
sushi <the.zcu@gmail.com>
2024-09-14 18:51:54 +0900

    feat/eduSystem914_3

--- src/main/java/com/example/edusystem/dao/GroupClassDao.java
+++ src/main/java/com/example/edusystem/dao/GroupClassDao.java
@@ -1,6 +1,5 @@
 package com.example.edusystem.dao;
 
-import com.example.edusystem.dto.CourseCategory;
 import com.example.edusystem.dto.GroupClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,8 +8,7 @@ import java.util.List;
 
 @Repository
 public interface GroupClassDao extends JpaRepository<GroupClass, Long> {
-    List<GroupClass> findByStudentNumber(String studentNumber);
 
-    List<GroupClass> findByStudentNumberIn(List<String> studentNumberList);
+    List<GroupClass> findByStudentNumberListIn(List<String> studentNumberList);
 
 }
